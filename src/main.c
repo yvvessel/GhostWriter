ac209ba4107ae8cabdf4010ddf65b1f27cccab4a
Thiago <thiago.dossantosgomes032006@gmail.com>
2026-08-13 01:28:53 -0300

    feat: add snippet creation

--- src/main.c
+++ src/main.c
@@ -2,20 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-typedef struct 
+typedef struct
 {
     char chave[50];
     char texto[1000];
 } Snippet;
 
+void criar_snippet();
+
 
 Snippet snippets[100];
 int quantidade = 0;
 
-int main (void)
+int main(void)
 {
     int opcao;
+
     while (1)
     {
         printf("======================\n");
@@ -31,12 +33,13 @@ int main (void)
         printf("Escolha (numero): ");
         scanf("%d", &opcao);
         getchar();
+
         switch (opcao)
         {
             case 1:
                 criar_snippet();
                 break;
-        
+
             case 2:
                 listar_snippets();
                 break;
@@ -55,9 +58,40 @@ int main (void)
 
             case 6:
                 return 0;
+
+            default:
+                printf("Opcao invalida.\n");
+                break;
         }
+
+        printf("\n");
+    }
+}
+
+void criar_snippet()
+{
+    if (quantidade >= 100)
+    {
+        printf("Limite de snippets atingido.\n");
+        return;
     }
 
+    printf("Chave: ");
+    fgets(snippets[quantidade].chave, 50, stdin);
 
+    snippets[quantidade].chave[
+        strcspn(snippets[quantidade].chave, "\n")
+    ] = '\0';
+
+    printf("Texto: ");
+    fgets(snippets[quantidade].texto, 1000, stdin);
+
+    snippets[quantidade].texto[
+        strcspn(snippets[quantidade].texto, "\n")
+    ] = '\0';
+
+    quantidade++;
+
+    printf("Snippet criado!\n");
 }
 
